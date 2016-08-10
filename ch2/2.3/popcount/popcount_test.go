18c9df48bb135def731dbd339a3f472395b81d05
Olivier Van Acker <cyberroadie@gmail.com>
2016-08-10 23:58:08 +0100

    benchmark tests with loop

--- ch2/2.3/popcount/popcount_test.go
+++ ch2/2.3/popcount/popcount_test.go
@@ -1,4 +1,4 @@
-// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+﻿// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
 // License: https://creativecommons.org/licenses/by-nc-sa/4.0/
 
 package popcount_test
