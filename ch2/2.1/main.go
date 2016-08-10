8b6cbe52def64625310f7e62162817de16d940b6
Olivier Van Acker <cyberroadie@gmail.com>
2016-08-10 21:40:15 +0100

    adding kelvin conversion

--- ch2/2.1/main.go
+++ ch2/2.1/main.go
@@ -24,8 +24,10 @@ func main() {
 		}
 		f := tempconv.Fahrenheit(t)
 		c := tempconv.Celsius(t)
-		fmt.Printf("%s = %s, %s = %s\n",
-			f, tempconv.FToC(f), c, tempconv.CToF(c))
+		k := tempconv.Kelvin(t)
+		fmt.Printf("%s = %s, %s = %s, %s = %s, %s = %s\n",
+			f, tempconv.FToC(f), c, tempconv.CToF(c),
+			k, tempconv.KToC(k), k, tempconv.KToF(k))
 	}
 }
 
