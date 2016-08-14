b78f8d8f0356293da82ab9b2f7d80080be6776a0
Olivier Van Acker <cyberroadie@gmail.com>
2016-08-14 22:43:47 +0100

    exercise 3.6 + images

--- ch3/3.6/mandlebrot.go
+++ ch3/3.6/mandlebrot.go
@@ -21,16 +21,30 @@ func main() {
 	const (
 		xmin, ymin, xmax, ymax = -2, -2, +2, +2
 		width, height          = 2048, 2048
+		subpixels              = 2
 	)
 
-	img := image.NewRGBA(image.Rect(0, 0, width, height))
+	valueArray := [][]color.Color{}
+
 	for py := 0; py < height; py++ {
 		y := float64(py)/height*(ymax-ymin) + ymin
+		row := []color.Color{}
 		for px := 0; px < width; px++ {
 			x := float64(px)/width*(xmax-xmin) + xmin
 			z := complex(x, y)
 			// Image point (px, py) represents complex value z.
-			img.Set(px, py, mandelbrot(z))
+			row = append(row, mandelbrot(z))
+		}
+		valueArray = append(valueArray, row)
+	}
+
+	img := image.NewRGBA(image.Rect(0, 0, width/2, height/2))
+	for py := 0; py < height; py = py + 2 {
+		for px := 0; px < width; px = px + 2 {
+
+			ac := avarageColor(valueArray[py][px], valueArray[py+1][px+0], valueArray[py+0][px+1], valueArray[py+1][px+1])
+
+			img.Set(px/2, py/2, ac)
 		}
 	}
 
@@ -41,6 +55,22 @@ func main() {
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
 	}
+}
+
+func avarageColor(color1, color2, color3, color4 color.Color) color.Color {
+	r1, g1, b1, _ := color1.RGBA()
+	r2, g2, b2, _ := color2.RGBA()
+	r3, g3, b3, _ := color3.RGBA()
+	r4, g4, b4, _ := color4.RGBA()
+
+	ac := color.RGBA{
+		uint8((r1 + r2 + r3 + r4) / 4),
+		uint8((g1 + g2 + g3 + g4) / 4),
+		uint8((b1 + b2 + b3 + b4) / 4),
+		255,
+	}
+
+	return ac
 }
 
 func mandelbrot(z complex128) color.Color {
