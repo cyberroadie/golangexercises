2e786649bad385d48160df6998d83b10f16c5d59
Olivier Van Acker <cyberroadie@gmail.com>
2016-09-18 12:13:47 +0100

    exercise 5.14 breadthfirst for courses

---
+++ ch5/5.14/breadthfirst.go
@@ -0,0 +1,61 @@
+package main
+
+import "fmt"
+
+//!+table
+// prereqs maps computer science courses to their prerequisites.
+var prereqs = map[string][]string{
+	"algorithms": {"data structures"},
+	"calculus":   {"linear algebra"},
+
+	"compilers": {
+		"data structures",
+		"formal languages",
+		"computer organization",
+	},
+
+	"data structures":       {"discrete math"},
+	"databases":             {"data structures"},
+	"discrete math":         {"intro to programming"},
+	"formal languages":      {"discrete math"},
+	"networks":              {"operating systems"},
+	"operating systems":     {"data structures", "computer organization"},
+	"programming languages": {"data structures", "computer organization"},
+}
+
+//!+breadthFirst
+// breadthFirst calls f for each item in the worklist.
+// Any items returned by f are added to the worklist.
+// f is called at most once for each item.
+func breadthFirst(f func(item string) []string, worklist []string) {
+	seen := make(map[string]bool)
+	for len(worklist) > 0 {
+		items := worklist
+		worklist = nil
+		for _, item := range items {
+			if !seen[item] {
+				seen[item] = true
+				worklist = append(worklist, f(item)...)
+			}
+		}
+	}
+}
+
+//!-breadthFirst
+
+//!+crawl
+func crawl(course string) []string {
+	fmt.Println(course)
+	return prereqs[course]
+}
+
+//!-crawl
+
+func main() {
+
+	keys := make([]string, 0, len(prereqs))
+	for k := range prereqs {
+		keys = append(keys, k)
+	}
+	breadthFirst(crawl, keys)
+}
